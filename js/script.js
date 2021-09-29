b4db6a1ed1f6bad4a5490f5f8c703efc7411d0b3
Afolabi Opakunle <opakunleafolabi@gmail.com>
2021-09-29 06:15:01 +0100

    cards displaying

--- js/script.js
+++ js/script.js
@@ -50,4 +50,19 @@ document.addEventListener('DOMContentLoaded', () => {
             img: 'images/milkshake.png'
         },
     ]
+
+    const grid = document.querySelector('.grid');
+
+    //create board
+    let createBoard = () => {
+        for(let i = 0; i < cardArray.length; i++) {
+            let card = document.createElement('img');
+            card.setAttribute('src', 'images/blank.png');
+            card.setAttribute('data-id', i);
+            // card.addEventListener('click', flipCard)
+            grid.appendChild(card)
+        }
+    }
+
+    createBoard()
 })
